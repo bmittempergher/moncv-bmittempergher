0699b8792a143406bdacc8c7150e9aedc2b97edf
Mittempergher Bastien <bastien.mittempe@he-arc.ch>
2017-06-27 20:06:22 +0200

    Version initial avec config jquery

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/cyborg/bootstrap.min.css';
+import './main.css';
+
+$(document).ready(() => {
+    console.log('it works!');
+});
+
