bf14f25429eea986f6ddc381e2d59817498e9ec8
bmittempergher <bastien.mittempe@he-arc.ch>
2017-06-28 21:11:35 +0200

    Debut chart.js et changement données personnelles

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import $ from 'jquery';
+import Chart from 'chart.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootswatch/superhero/bootstrap.min.css';
+import 'bootswatch/readable/bootstrap.min.css';
 import './main.css';
 
 $(document).ready(() => {
     console.log('it works!');
+    let monChart;
+    let ctx = document.getElementsByClassName("progress");
+    monChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: Chart.defaults.doughnut
+    }
+    );
 });
-
