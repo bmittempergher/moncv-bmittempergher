b99758fa4e20d6149805d1d5cf7d985df1a8d93b
bmittempergher <bastien.mittempe@he-arc.ch>
2017-06-29 11:05:15 +0200

    Changement structure d'index.html et ajout de code dans main.js Committer: bmittempergher <bastien.mittempe@he-arc.ch> 	modified:   index.html 	modified:   src/main.js
    Index : modification de la balise meta
    	changement section compétence. tableau -> div de class : row et col-x-x
    
    Main.js : Ajout des fonctions pour la transformation de page
    		-> progress bar en chart

--- src/main.js
+++ src/main.js
@@ -7,22 +7,80 @@ import './main.css';
 
 $(document).ready(() => {
     console.log('it works!');
-    let monChart;
-    let ctx = document.getElementsByClassName("progress");
+    /* let monChart;
+    $('.progress').replaceWith('<canvas id="graf" width="50px" height="50px"></canvas>');
+    let ctx = $('#graf');
     monChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
             datasets: [{
+                data: [6, 4],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)'
+                    'rgba(0, 222, 0, 0.2)',
+                    'rgba(0, 0, 222, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(255,99,132,1)'
+                    'rgba(222,0,0,1)',
+                    'rgba(222,0,0,1)'
                 ],
                 borderWidth: 1
             }]
         },
         options: Chart.defaults.doughnut
+    });
+    ctx = monChart; */
+    /* let ctx = $('.progress');
+    ctx.each(function (index) {
+        $('.progress-bar').each(function (index) {
+            let val = index.attr('aria-valuenow');
+            ctx.replaceWith(val);
+        });
+    }); */
+    // Recherche des éléments avec la class progress
+    let listProg = document.getElementsByClassName('progress');
+    // Extraction des infos de la progress bar
+    function extractDataFromProgressBar (progress) {
+        const bar = progress.children[0];
+        const val = bar.getAttribute('aria-valuenow') * 10;
+        return val;
     }
-    );
+    // Création des canvas
+    function createCanvas () {
+        const monCanvas = $('<canvas class="canvas"></canvas>');
+        return monCanvas;
+    }
+    // Dessine le chart de remplacement des progress bar
+    function drawChart (val, canvas) {
+        let ctx = $('.canvas');
+        const monChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ['Savoir', 'Inconnu'],
+                datasets: [{
+                    data: [val, (100 - val)],
+                    backgroundColor: [
+                        '#2196F3',
+                        '#E3F2FD'
+                    ],
+                    borderColor: [
+                        '#0D47A1',
+                        '#0D47A1'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: Chart.defaults.doughnut
+        });
+        return monChart;
+    }
+    // Parcours de la liste des progress bar et fais les transformations
+    function transformPage () {
+        for (let progress of listProg) {
+            const val = extractDataFromProgressBar(progress);
+            const monCanvas = createCanvas();
+            const monChart = drawChart(val, monCanvas);
+            progress.replaceWith($(monChart));
+        }
+    }
+    transformPage();
 });
